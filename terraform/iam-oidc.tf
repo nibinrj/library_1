b428c4dcbda4df3a0ca27c692b67491cf6dabdf0
nibinrj <nsellaraj@gmail.com>
2026-08-14 22:06:53 +0530

    patch: terraform plan - 4

--- terraform/iam-oidc.tf
+++ terraform/iam-oidc.tf
@@ -11,17 +11,9 @@
     # TRUST POLICY — who is allowed to assume the role
     # Scoped to your repo + main branch only.
     ############################################
-    resource "aws_iam_openid_connect_provider" "github_actions" {
-      url = "https://token.actions.githubusercontent.com"
-
-      client_id_list = [
-        "sts.amazonaws.com"
-      ]
-
-      thumbprint_list = [
-        "6938fd4d98bab03faadb97b34396831e3780aea1"
-      ]
-    }
+ data "aws_iam_openid_connect_provider" "github_actions" {
+   url = "https://token.actions.githubusercontent.com"
+ }
 
     data "aws_iam_policy_document" "github_actions_trust" {
       statement {
@@ -29,9 +21,9 @@
         actions = ["sts:AssumeRoleWithWebIdentity"]
 
         principals {
-          type        = "Federated"
-          identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
-        }
+      type        = "Federated"
+      identifiers = [data.aws_iam_openid_connect_provider.github_actions.arn]
+    }
 
         condition {
           test     = "StringEquals"
